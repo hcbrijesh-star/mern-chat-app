f769a41327afb61651a79d4b2fd1ece6f1a5712a
Brijesh HC <yourgmail@gmail.com>
2026-05-12 21:58:37 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -43,11 +43,11 @@ if (process.env.NODE_ENV === "production") {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(`Server running on PORT ${PORT}...`.yellow.bold)
+  
 );
 
 const io = require("socket.io")(server, {
